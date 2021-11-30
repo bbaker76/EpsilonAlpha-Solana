1ad6d36acd806bec44c40882c026d88f9bfa83b0
Bradley Baker <50665649+bbaker76@users.noreply.github.com>
2021-11-30 03:37:42 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      React⚛️ + Vite⚡ + Replit🌀 
+      
+    </main>
+
+  );
+
+
+}
+
+
+export default App;
